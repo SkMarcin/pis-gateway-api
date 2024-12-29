e4897475a37170f6c247267f86dabae04df6e2ad
msikorsk <01178592@pw.edu.pl>
2024-12-29 03:06:36 +0100

    Moved JWT authentication

--- src/main/java/com/example/gateway_api/GatewayApiApplication.java
+++ src/main/java/com/example/gateway_api/GatewayApiApplication.java
@@ -2,9 +2,6 @@ package com.example.gateway_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 
 @SpringBootApplication
 public class GatewayApiApplication {
@@ -12,15 +9,5 @@ public class GatewayApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-				.route(p -> p
-						.path("/auth/**")
-						.uri("http://localhost:8005"))
-				.build();
-	}
 }
-
 
